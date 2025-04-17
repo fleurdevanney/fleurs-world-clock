ef77320e784c984bfa863ba7734a6289dd08b627
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-04-17 12:30:41 +0100

    updated js
    updated js code so it presents the correct data. added setInterval to refresh the time data for better UX

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector("#date");
-let londonTimeElement = londonElement.querySelector("#time");
+setInterval(function () {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector("#date");
+  let londonTimeElement = londonElement.querySelector("#time");
 
-londonDateElement.innerHTML = moment()
-  .tz("Europe / London")
-  .format("dddd, Do MMMM");
+  londonDateElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("dddd, Do MMMM");
 
-londonTimeElement.innerHTML = moment().tz("Europe/London").format("HH:mm:ss A");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("HH:mm:ss A");
+}, 10);
 
-let madridElement = document.querySelector("#madrid");
-let madridDateElement = londonElement.querySelector("#date");
-let madridTimeElement = londonElement.querySelector("#time");
+setInterval(function () {
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector("#date");
+  let madridTimeElement = madridElement.querySelector("#time");
 
-madridDateElement.innerHTML = moment()
-  .tz("Europe / Madrid")
-  .format("dddd, Do MMMM");
+  madridDateElement.innerHTML = moment()
+    .tz("Europe/Madrid")
+    .format("dddd, Do MMMM");
 
-madridTimeElement.innerHTML = moment().tz("Europe/Madrid").format("HH:mm:ss A");
+  madridTimeElement.innerHTML = moment()
+    .tz("Europe/Madrid")
+    .format("HH:mm:ss A");
+}, 10);
+
+setInterval(function () {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector("#date");
+  let newYorkTimeElement = newYorkElement.querySelector("#time");
+
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("dddd, Do MMMM");
+
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("HH:mm:ss A");
+}, 10);
